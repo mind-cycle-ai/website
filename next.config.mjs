f30ee5d7cb4f0e8f8abb8d8cf38e2cafde5320ad
Dwight Ware <dwightware4@gmail.com>
2025-04-14 23:43:00 -0700

    Comment out MDX configuration in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
-import rehypePrism from '@mapbox/rehype-prism'
-import nextMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
+// import rehypePrism from '@mapbox/rehype-prism'
+// import nextMDX from '@next/mdx'
+// import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
-})
+// const withMDX = nextMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     remarkPlugins: [remarkGfm],
+//     rehypePlugins: [rehypePrism],
+//   },
+// })
 
-export default withMDX(nextConfig)
+// export default withMDX(nextConfig)
